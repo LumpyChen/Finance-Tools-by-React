d7f541666b12fda6c61bb00d65bd3d0c256134c2
Chen Yiwen <lumpychen@outlook.com>
2016-03-29 22:02:16 +0800

    window.verify

--- components/Yuebao.js
+++ components/Yuebao.js
@@ -9,6 +9,7 @@ import Loan from './Loan'
 import Solution from './Yuebaosolution'
 
 export default class Yuebao extends React.Component {
+
     constructor(props){
         super(props);
         this.state = {
@@ -21,13 +22,11 @@ export default class Yuebao extends React.Component {
             sixth:"",
             seventh:"",
             submit:false,
-            verify:"empty"
         }
     }
-    componentDidMount(){
-        this.refs.cal.disabled = "disabled";
-    }
-    componentDidUpdate(){
+    componentWillUpdate(nexP,nexS){
+        console.log('in'+this.state.verify)
+        console.log(nexS.verify+'+')
         let arr = [
             this.state.first,
             this.state.second,
@@ -37,44 +36,53 @@ export default class Yuebao extends React.Component {
             this.state.sixth,
             this.state.seventh
         ]
-        let empty = true;
+        //console.log(arr);
         var that = this
         function changeMsg (ver){
             switch (ver) {
                 case 'empty':
-                    that.refs.message.className = 'col-md-offset-1 col-sm-offset-1 help-block'
+                    that.refs.message.className = 'col-md-offset-1 col-sm-offset-1 text-default'
                     that.refs.message.innerHTML = "请输入您的金融数据。"
+                    document.getElementById('trans').disabled = true;
                     break;
                 case 'error':
                     that.refs.message.className = 'col-md-offset-1 col-sm-offset-1 text-danger'
                     that.refs.message.innerHTML = "您输入的金融数据有误。"
+                    document.getElementById('trans').disabled = true;
                     break;
                 case 'pass':
                     that.refs.message.className = 'col-md-offset-1 col-sm-offset-1 text-success'
                     that.refs.message.innerHTML = "您输入的金融数据成立。"
+                    document.getElementById('trans').disabled = false;
             }
         }
-
+        let state = "";
         for (let i = 0;i<arr.length;i++) {
-            if (arr[i] == "") {
-                this.state.verify = "empty"
-                console.log(this.refs.message)
-                break;
-            } else if (!(!Number.isNaN(arr[i]) && (arr[i] >= 0) && (20 > arr[i])))  {
-                this.state.verify = "error"
-                break;
-            } else {
-                this.state.verify = "pass"
-                this.state.submit = true;
+            if (!((this.state.gross>=0) && (!Number.isNaN(this.state.gross)) && (!Number.isNaN(arr[i])) && (arr[i] >= 0) && (20 > arr[i]) ))  {
+                state = "error"
+                //console.log('err');
+            } else if ((arr[i] == "") || (this.state.gross == '')) {
+                if (state!="error"){
+                    state = "empty"
+                    //console.log('empty');
+                }
+            }
+            else {
+                if((state!="error")&&(state!="empty")){
+                    state = "pass"
+                }
+                //console.log('pass');
             }
         }
+        window.verify = state;
+        console.log('out'+state);
+        changeMsg(state);
 
-        changeMsg(this.state.verify)
     }
 
     handleSubmit(e){
         this.setState({
-            submit:!this.state.submit
+            submit:true
         })
         console.log(this.state.submit)
     }
@@ -94,6 +102,7 @@ export default class Yuebao extends React.Component {
 
 
     render(){
+        console.log(window.verify)
         return (
             <div className="row" id="menu">
                 <ul className=" col-md-offset-1 col-md-5 row">
@@ -106,55 +115,57 @@ export default class Yuebao extends React.Component {
                         <div className="panel-body ">
                             <form role="form" className="form-horizontal" action="">
                                 <div className="form-group">
-                                    <div className="col-md-10 col-sm-offset-1 col-sm-10">
-                                        <div className="form-group">请输入您今日在余额宝中的余额总额</div>
+                                    <div className="">
+                                        <label forHtml="gross" className="col-md-12 col-sm-12 ">请输入您今日在余额宝中的余额总额</label>
                                     </div>
-                                    <div className="col-md-10 col-sm-offset-1 col-sm-10">
-                                        <input className="form-control" valueLink={linkState(this,'gross')} type="text" placeholder="请输入余额"/>
+                                    <div className="col-md-12 col-sm-12">
+                                        <input id="gross" className="form-control" valueLink={linkState(this,'gross')} type="text" placeholder="请输入余额"/>
                                     </div>
                                 </div>
-                                <div className="form-group">
-                                    <div className="col-sm-offset-1 help-block" >请输入<a href="">天弘基金</a>过去七日的每万份收益以进行计算：</div>
-                                    <div className="col-sm-offset-1 help-block" ><a href="">点击此处查询天弘基金的每万份收益</a></div>
+
+                                <hr/>
+                                <div className="form-group text-center">
+                                    <label className=" col-md-12 col-sm-12" >请输入<a href="http://www.thfund.com.cn">天弘基金</a>过去七日的每万份收益以进行计算：</label>
+                                    <label className=" col-md-12 col-sm-12" ><a href="http://fund.eastmoney.com/000198.html?spm=aladin">点击此处查询天弘基金的每万份收益</a></label>
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="first" className="col-md-3 control-label">前第一日:</label>
-                                    <div className="col-md-8">
+                                    <div className="col-md-9">
                                         <input id="first" name="firth" valueLink={linkState(this,'first')}  type="text" placeholder="天弘基金昨日每万份收益" className="form-control"/>
                                     </div>
                                 </div>
 
                                 <div className="form-group">
                                     <label htmlFor="second" className="col-md-3 control-label">前第二日:</label>
-                                    <div className="col-md-8">
+                                    <div className="col-md-9">
                                         <input name="second" id="second" valueLink={linkState(this,'second')} type="text" placeholder="天弘基金前日每万份收益" className="form-control"/>
                                     </div>
                                 </div>
 
                                 <div className="form-group">
                                     <label htmlFor="third" className="col-md-3 control-label">前第三日:</label>
-                                    <div className="col-md-8">
+                                    <div className="col-md-9">
                                         <input name="third" id="third" valueLink={linkState(this,'third')} type="text" placeholder="天弘基金前第三日每万份收益" className="form-control"/>
                                     </div>
                                 </div>
 
                                 <div className="form-group">
                                     <label htmlFor="fourth" className="col-md-3 control-label">前第四日:</label>
-                                    <div className="col-md-8">
+                                    <div className="col-md-9">
                                         <input name="fourth" id="fourth" valueLink={linkState(this,'fourth')} type="text" placeholder="天弘基金前第四日每万份收益" className="form-control"/>
                                     </div>
                                 </div>
 
                                 <div className="form-group">
                                     <label htmlFor="fifth" className="col-md-3 control-label">前第五日:</label>
-                                    <div className="col-md-8">
+                                    <div className="col-md-9">
                                         <input name="fifth" id="fifth" valueLink={linkState(this,'fifth')} type="text" placeholder="天弘基金前第五日每万份收益" className="form-control"/>
                                     </div>
                                 </div>
 
                                 <div className="form-group">
                                     <label htmlFor="sixth" className="col-md-3 control-label">前第六日:</label>
-                                    <div className="col-md-8">
+                                    <div className="col-md-9">
                                         <input name="sixth" id="sixth" type="text" valueLink={linkState(this,'sixth')} placeholder="天弘基金前第六日每万份收益" className="form-control"/>
                                     </div>
                                 </div>
@@ -162,20 +173,20 @@ export default class Yuebao extends React.Component {
 
                                 <div className="form-group">
                                     <label htmlFor="seventh" className="col-md-3 control-label">前第七日:</label>
-                                    <div className="col-md-8">
+                                    <div className="col-md-9">
                                         <input name="seventh" id="seventh" valueLink={linkState(this,'seventh')} type="text" placeholder="天弘基金前第七日每万份收益" className="form-control"/>
                                     </div>
                                 </div>
 
                                 <div className="form-group">
-                                    <p ref="message" className="col-md-offset-1 help-block col-sm-offset-1">请输入您的金融数据。</p>
+                                    <p ref="message" className="col-md-offset-1 text-default col-sm-offset-1">请输入您的金融数据。</p>
                                 </div>
 
 
 
                             </form>
                             <div className="row">
-                                <button name="main" ref="cal" onClick={ (e) => {this.handleSubmit(e);}  }  ref="cal"  className="btn col-sm-12 col-md-offset-1 col-md-4 btn-success  text-center">
+                                <button name="main" id="trans" onClick={ (e) => {this.handleSubmit(e);}  }  ref="cal"  className="btn col-sm-12 col-md-offset-1 col-md-4 btn-success  text-center">
                                     Translate
                                 </button>
                                 <button name="main" onClick={ (e) => {this.handleClick(e);}  } className="btn col-md-offset-2 col-sm-12 col-md-4 btn-primary text-center">
@@ -196,7 +207,8 @@ export default class Yuebao extends React.Component {
                             linkState(this,'fifth'),
                             linkState(this,'sixth'),
                             linkState(this,'seventh'),
-                            linkState(this,'submit')
+                            linkState(this,'submit'),
+                            {value:window.verify}
                         ]
                     }  />
                 </div>
@@ -205,3 +217,4 @@ export default class Yuebao extends React.Component {
         )
     }
 }
+
--- components/Yuebaosolution.js
+++ components/Yuebaosolution.js
@@ -9,6 +9,10 @@ import linkState from 'react-link-state';
 export default class Yuebaosolution extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            yearVal:0,
+            radio:0
+        }
 
     }
 
@@ -16,49 +20,65 @@ export default class Yuebaosolution extends React.Component {
         this.refs.solu_root.style.display = "none";
     }
 
-    componentWillReceiveProps(nextprops){
+    shouldComponentUpdate(nextprops,nextstate){
+        console.log(nextprops.value[9].value);
         if (nextprops.value[8].value){
             //console.log(this.refs.solu_root.style.display)
             this.refs.solu_root.style.display = "block"
             //console.log(this.refs.solu_root.style.display)
         }
         else { this.refs.solu_root.style.display = "none"}
+        if(nextprops.value[9].value=='pass'){
+            let arr = nextprops.value.filter(function (ele, index) {
+                return !(index == 0 || index == 8 || index == 9)
+            })
+
+            //console.log(arr)
+
+            let arrVal = arr.map((ele)=>
+                ele.value
+            )
+
+            //console.log(arrVal)
+
+            let sum = arrVal.reduce(function (pre, cur) {
+                //console.log('pre'+pre)
+                return pre * (1 + (cur / 10000))
+            }, 1)
+
+            //console.log(sum)
+
+            var yearVal = ((Math.pow(sum, 365 / 7) - 1) * 100).toFixed(2);
+            var ratio = Math.pow(sum, 365 / 7);
+
+            //console.log(yearVal)
+            //console.log(ratio)
+
+            this.state.yearVal = yearVal;
+            this.state.ratio = ratio;
+
+            return true;
+        }else{
+            return false;
+        }
+
     }
 
     render(){
 
-        //console.log(this.props.value[0])
 
-        let arr = this.props.value.filter(function(ele,index){
-            return !(index==(0||8||9))
-        })
+            return (
+                <div ref="solu_root" className="panel panel-default">
+                    <div className="panel-heading">结果</div>
+                    <div className="panel-body">
+                        <p>七日年化收益率为:{this.state.yearVal}%</p>
+                        <p>按照您的目前在余额宝的本金,</p>
+                        <p>如果天弘基金的收益和前七天的趋势相同,</p>
+                        <p>那么一年后您余额宝里的金额为:</p>
+                        <h1>{(this.props.value[0].value * this.state.ratio).toFixed(2)}</h1>
+                    </div>
+                </div>
+            )
+        }
 
-        //console.log(arr)
-
-        let arrVal = arr.map((ele)=>
-            Math.round(ele.value*1000)/1000
-        )
-
-        //console.log(arrVal)
-
-        let sum = arrVal.reduce(function(pre,cur) {
-            //console.log('pre'+pre)
-            return pre*(1+cur/10000)
-        },1)
-
-        //console.log(sum)
-
-        let yearVal = Math.round(Math.pow(sum,365/7)*1000)/1000
-        console.log(Math.round(yearVal))
-
-
-
-        return (
-            <div ref="solu_root" >
-                <p>七日年化收益率为:{yearVal}</p>
-                <p>按照您的目前在余额宝的本金,如果天弘基金的收益和前七天的趋势相同,那么一年后您余额宝里的金额为:</p>
-                <h1>{Math.round(this.props.value[0].value*yearVal*100)/100}</h1>
-            </div>
-        )
-    }
 }
